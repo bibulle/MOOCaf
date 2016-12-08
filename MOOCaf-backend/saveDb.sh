8da2df0578fc18f64742089b5cefd8ae336dbe43
bibulle <famille.martin@gmail.com>
2016-12-08 14:46:42 +0100

    Add script to save data

---
+++ MOOCaf-backend/saveDb.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+then
+   echo "+++"
+   echo "+++ Usage : $0 <mongoDb host>"
+   echo "+++"
+   exit -1
+fi
+HOST=$1
+
+MONGOEXPORT=~/Documents/mongodb/bin/mongoexport
+
+SAVE_PATH=saves/`date +%Y_%m_%d`
+
+mkdir ${SAVE_PATH} 2>/dev/null
+
+for col in awards courses usercourses users userstats
+do
+  ${MONGOEXPORT} --host ${HOST} --db MOOCer --collection ${col} --out ${SAVE_PATH}/${col}.json
+done
+
